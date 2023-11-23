1a688829194fca009c4c800f48f4f5391c4aab90
Siddhesh Agarwal <siddhesh.agarwal@gmail.com>
2023-11-23 16:58:43 +0530

    Refactor file upload endpoint

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-var express = require('express');
-var cors = require('cors');
+const express = require('express');
+const multer = require('multer');
+const cors = require('cors');
 require('dotenv').config()
 
-var app = express();
+const app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -10,8 +11,19 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
+const upload = multer({ dest: 'uploads/' })
 
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  if (!req.file) {
+    res.json({ error: 'No file selected' })
+  }
 
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  })
+});
 
 
 const port = process.env.PORT || 3000;
